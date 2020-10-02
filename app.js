cdea289f061bde455975bda87273b251206ddf30
Sumit Gurung <sumitgrg24325@gmail.com>
2020-10-02 21:35:03 +0545

    category-page

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#ffffff"
+        "value": "#154996"
       },
       "shape": {
         "type": "circle",
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#ffffff",
+        "color": "#27ABEF",
         "opacity": 0.4,
         "width": 1
       },
